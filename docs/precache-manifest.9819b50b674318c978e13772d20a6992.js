86ae4aaf76f4a10fcdec0109ce97a170a11af88e
Leonel <lazcano.leonel@gmail.com>
2020-07-26 16:32:54 -0300

    docs agregado

---
+++ docs/precache-manifest.9819b50b674318c978e13772d20a6992.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "106c83ca7d15996ffc686fdb948f1a06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f54976c380da641be9c1",
+    "url": "/static/css/main.7fb08405.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f54976c380da641be9c1",
+    "url": "/static/js/main.c6f0ee61.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
